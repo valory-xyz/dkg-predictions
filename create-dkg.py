e7a5818316b36e7642a5b6eb437ccf7f1cac0ba3
Richard Blythman <richardblythman@gmail.com>
2024-01-18 21:12:13 +0000

    custom dkg working

--- create-dkg.py
+++ create-dkg.py
@@ -1,5 +1,5 @@
 import json
-import os 
+import os
 from dotenv import load_dotenv
 
 from dkg import DKG
@@ -8,56 +8,41 @@ from dkg.providers import BlockchainProvider, NodeHTTPProvider
 load_dotenv()
 
 API_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MDQ5NzI5NjQsImV4cCI6MTcyMDc1MTc2NCwianRpIjoiNTYyYzVjYzUtZjIxMC00ZTFlLWI0MDYtNzc0NDA5NzhmNWY0In0.xo1Z81-mGsDBQBGADftNXrjCYk6GUxjwQinE3Bdv1IA"
-ENVIRONMENT = "devnet"
+ENVIRONMENT = "testnet"
 OT_NODE_ENDPOINT = "https://v6-pegasus-node-19.origin-trail.network:8900"
 PRIVATE_KEY = os.getenv("PRIVATE_KEY")
 
-node_provider = NodeHTTPProvider(
-    endpoint_uri=OT_NODE_ENDPOINT,
-    auth_token=API_TOKEN
-)
+node_provider = NodeHTTPProvider(endpoint_uri=OT_NODE_ENDPOINT, auth_token=API_TOKEN)
 
 blockchain_provider = BlockchainProvider(
-    ENVIRONMENT,
-    blockchain_id="gnosis:10200",
-    private_key=PRIVATE_KEY
+    ENVIRONMENT, blockchain_id="gnosis:10200", private_key=PRIVATE_KEY
 )
 
 dkg = DKG(node_provider, blockchain_provider)
 
-# Read knowledge assets
-with open("./knowledge-assets/tool.json", "r") as file:
-    tool_asset = json.load(file)
+print("Node Info: ", dkg.node.info)
 
-with open("./knowledge-assets/market.json", "r") as file:
-    market_asset = json.load(file)
+content = {
+    "@context": "https://schema.org",
+    "@id": "0x0094fa304017d5c2b355790e2976f769ea600492",
+    "title": "Will the Hisense U8K be considered a top-tier screen for well-lit rooms by 22 August 2023?",
+    "@type": "Market",
+    "Prediction": {
+        "@type": "Prediction",
+        "@id": "46575543203365495795461826533331162746381515212880293824967167776593568770457",
+        "result": "Yes",
+        "tool": {
+            "@type": "Tool",
+            "name": "prediction-online",
+        },
+    },
+}
 
-with open("./knowledge-assets/prediction.json", "r") as file:
-    prediction_asset = json.load(file)
-
-public_assertion_id = dkg.assertion.get_public_assertion_id(content)
-print("Public Assertion ID", public_assertion_id)
-
-public_assertion_size = dkg.assertion.get_size(content)
-print("Public Assertion Size", public_assertion_size)
-
-bid_suggestion = dkg.network.get_bid_suggestion(
-    public_assertion_id,
-    public_assertion_size,
-    2,
+create_asset_result = dkg.asset.create(
+    {
+        "public": content,
+    },
+    epochs_number=2,
 )
-print("Bid Suggestion: ", bid_suggestion)
-
-current_allowance = dkg.asset.get_current_allowance()
-print("Current Allowance: ", current_allowance)
-
-allowance_increase = dkg.asset.increase_allowance(bid_suggestion)
-print("Increased Allowance: ", allowance_increase)
-
-print("Creating asset...")
-create_asset_result = dkg.asset.create(content, 2)
 
 print(create_asset_result)
-
-
-
